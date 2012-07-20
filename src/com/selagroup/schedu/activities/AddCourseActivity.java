59b94b5f73b5a3964ff63c2bcb813cb04c390021
Mark Redden <markr@selagroup.com>
2012-07-19 22:11:29 -0700

    addcourse layout and activity

--- src/com/selagroup/schedu/activities/AddCourseActivity.java
+++ src/com/selagroup/schedu/activities/AddCourseActivity.java
@@ -8,17 +8,15 @@ package com.selagroup.schedu.activities;
 import com.selagroup.schedu.R;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
 /**
  * The Class TermActivity.
  */
-public class TermActivity extends Activity {
+public class AddCourseActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_term);
-		startActivity(new Intent(this, AddCourseActivity.class));
+		setContentView(R.layout.activity_addcourse);
 	}
 }
