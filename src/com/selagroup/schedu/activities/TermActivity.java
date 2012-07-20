21d9468f214e2deed7637c17054ef8522f8fd284
Mark Redden <marred01@hotmail.com>
2012-07-19 18:00:43 -0700

    changes for term layout, remove default files, add term activity

---
+++ src/com/selagroup/schedu/activities/TermActivity.java
@@ -0,0 +1,22 @@
+/**
+ * @author Nick Huebner and Mark Redden
+ * @version 1.0
+ */
+
+package com.selagroup.schedu.activities;
+
+import com.selagroup.schedu.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * The Class TermActivity.
+ */
+public class TermActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_term);
+	}
+}
