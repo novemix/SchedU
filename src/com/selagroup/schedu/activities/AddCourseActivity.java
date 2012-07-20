781a865452ccfc0c1c7736c70d83ba319969a194
Mark Redden <markr@selagroup.com>
2012-07-19 23:07:00 -0700

    start on add time dialog

--- src/com/selagroup/schedu/activities/AddCourseActivity.java
+++ src/com/selagroup/schedu/activities/AddCourseActivity.java
@@ -8,8 +8,12 @@ package com.selagroup.schedu.activities;
 import com.selagroup.schedu.R;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 /**
  * The Class AddCourseActivity.
  */
@@ -18,5 +22,24 @@ public class AddCourseActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_addcourse);
+		
+		((Button) findViewById(R.id.addcourse_btn_add_time)).setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				myDialog d = new myDialog(AddCourseActivity.this);
+				d.show();
+				
+			}
+		});
+		
+	}
+	
+	public class myDialog extends Dialog {
+
+		public myDialog(Context context) {
+			super(context);
+			setContentView(R.layout.dialog_addtime);
+		}
+		
 	}
 }
