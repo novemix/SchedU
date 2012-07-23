71b74ffdc5f565151fc1874a9f85a715ec456d0b
Mark Redden <marred01@hotmail.com>
2012-07-23 15:50:03 -0700

    Instructor activity and some housekeeping

--- src/com/selagroup/schedu/activities/CourseActivity.java
+++ src/com/selagroup/schedu/activities/CourseActivity.java
@@ -8,7 +8,11 @@ package com.selagroup.schedu.activities;
 import com.selagroup.schedu.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 /**
  * The Class CourseActivity.
@@ -19,5 +23,12 @@ public class CourseActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_course);
+		
+		((Button) findViewById(R.id.course_btn_edit_instructor)).setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				startActivity(new Intent(CourseActivity.this, InstructorActivity.class));
+			}
+		});
 	}
 }
---
+++ src/com/selagroup/schedu/activities/InstructorActivity.java
@@ -0,0 +1,59 @@
+/**
+ * @author Nick Huebner and Mark Redden
+ * @version 1.0
+ */
+package com.selagroup.schedu.activities;
+
+import com.selagroup.schedu.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * The Class InstructorActivity.
+ */
+public class InstructorActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_instructor);
+		
+		LinearLayout hours = (LinearLayout) findViewById(R.id.instructor_ll_hours);
+		TextView tv1 = new TextView(this);
+		tv1.setText("Mon, Wed -- 1pm - 3pm");
+		TextView tv2 = new TextView(this);
+		tv2.setText("Tue, Thu -- 3pm-4pm");
+		TextView tv3 = new TextView(this);
+		tv3.setText("Fri -- 11am - 12pm");
+		TextView tv4 = new TextView(this);
+		tv4.setText("Sat -- 11am - 12pm");
+		TextView tv5 = new TextView(this);
+		tv5.setText("Sat -- 11am - 12pm");
+		TextView tv6 = new TextView(this);
+		tv6.setText("Sat -- 11am - 12pm");
+		TextView tv7 = new TextView(this);
+		tv7.setText("Sat -- 11am - 12pm");
+		hours.addView(tv1);
+		hours.addView(tv2);
+		hours.addView(tv3);
+		hours.addView(tv4);
+		hours.addView(tv5);
+		hours.addView(tv6);
+		hours.addView(tv7);
+		
+		((Button) findViewById(R.id.instructor_btn_add_time)).setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				startActivity(new Intent(InstructorActivity.this, AddTimeActivity.class));
+				
+			}
+		});
+	}
+}
