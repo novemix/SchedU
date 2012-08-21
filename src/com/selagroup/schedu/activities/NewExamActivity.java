be188b14d8dddf8c812c32a36ab8daf4640b1a91
Mark Redden <marred01@hotmail.com>
2012-08-21 08:31:19 -0700

    small cleanup

--- src/com/selagroup/schedu/activities/NewExamActivity.java
+++ src/com/selagroup/schedu/activities/NewExamActivity.java
@@ -16,7 +16,6 @@ import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.format.Time;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
